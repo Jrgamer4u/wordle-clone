a100d5c87811565cea6ae0b27231fc97dcdc4dc6
JRGAMER4U <jrgamer4u@gmail.com>
2025-10-12 20:35:54 -0700

    done

--- json/filter.py
+++ json/filter.py
@@ -3,8 +3,8 @@ import json
 with open('./json/dictionary.json', 'r') as file:
     data = json.load(file)
 length = 5
-data = ([word for word in data if len(word) == length])
+data = ([word.lower() for word in data if len(word) == length])
 with open('./json/targetWords.json', 'w', encoding='utf-8') as f:
-    json.dump(data, f, ensure_ascii=False, indent=4)
+    json.dump(sorted(data), f, ensure_ascii=False, indent=4)
 with open('./json/targetLetters.json', 'w', encoding='utf-8') as f:
-    json.dump(list(set(''.join(data))), f, ensure_ascii=False, indent=4)
+    json.dump(sorted(list(set(''.join(data).upper()))), f, ensure_ascii=False, indent=4)
